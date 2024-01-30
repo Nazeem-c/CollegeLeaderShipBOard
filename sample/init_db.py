6fd2c207061f5e579ca9a3741f99fcb1be6a3093
Nazeem-c <mhdnazeemc@gmail.com>
2024-01-30 14:00:35 +0530

    demojan30

--- sample/init_db.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify, request
-import psycopg2
-
-app = Flask(__name__)
-
-def db_conn():
-    conn = psycopg2.connect(database = "LeaderShipBoard",host="localhost",user="postgres",password="postgres",port="5432");
-    return conn
